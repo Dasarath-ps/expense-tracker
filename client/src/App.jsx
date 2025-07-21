a34a506981900421cd7ee3a5a45b2e1907c123d8
Dhasarath-ps <dhasarathps@gmail.com>
2025-07-21 06:47:11 +0530

    Auth routes are defined

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,37 @@
 import React from "react";
-
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import { useEffect } from "react";
 const App = () => {
-  return <div>app</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
+
+const Root = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const isAuthenticated = !!localStorage.getItem("token");
+    if (isAuthenticated) {
+      return navigate("/dashboard");
+    } else {
+      return navigate("/login");
+    }
+  }, [navigate]);
+};
