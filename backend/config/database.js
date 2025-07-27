bf9c479727bc8e88f68ba08f4d698ee6abeadd3c
Dhasarath-ps <dhasarathps@gmail.com>
2025-07-27 19:57:33 +0530

    set correct database name

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectionDb = () => {
   try {
     mongoose.connect(process.env.MONGO_URL);
-    console.log("Databse is connected");
+    console.log(`Databse is connected ${process.env.DATABASE_NAME}`);
   } catch (error) {
     console.log("Error", error);
   }
